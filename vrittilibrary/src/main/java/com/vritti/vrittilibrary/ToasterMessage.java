fb5c07858369c941b0c62a881b0cd513e0ae78ca
Pravin <pravinkunnure9@gmail.com>
2021-07-30 20:08:42 +0530

    Log added for testing

--- vrittilibrary/src/main/java/com/vritti/vrittilibrary/ToasterMessage.java
+++ vrittilibrary/src/main/java/com/vritti/vrittilibrary/ToasterMessage.java
@@ -1,6 +1,7 @@
 package com.vritti.vrittilibrary;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 public class ToasterMessage {
@@ -8,6 +9,7 @@ public class ToasterMessage {
     public static void s(Context c, String message){
 
         Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+        Log.e("MESSAGE",message);
 
     }
 }
